c7230bc6dfe3faebd7023e2b6db806ca45afdb36
DeckerSU <support@decker.su>
2019-12-27 04:31:58 +0300

    build update

--- run-solo.sh
+++ run-solo.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 STRATUM_CONFIG=equihash.conf
 # Build 
-make -C iniparser
-make -C algos
-make -f Makefile 
+make -C iniparser -j$(nproc)
+make -C sha3 -j$(nproc)
+make -C algos -j$(nproc)
+make -f Makefile -j$(nproc)
 # Prepare working directory
 mkdir -p workdir
 if [ ! -f "workdir/${STRATUM_CONFIG}" ]; then
